f766f0f2040ce1794569b401b273627ff09b83d8
sdhekane-iu <sdhekane@iu.edu>
2026-03-01 23:49:15 -0500

    duck testupdate

--- src/test/java/edu/iu/habahram/ducksservice/model/DuckTest.java
+++ src/test/java/edu/iu/habahram/ducksservice/model/DuckTest.java
@@ -1,25 +1,41 @@
 package edu.iu.habahram.ducksservice.model;
 
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class DuckTest {
 
     @Test
     void toLine() {
-        DuckData duck = new DuckData(1, "mallardly" , DuckType.MALLARD.toString());
+        DuckData duck = new DuckData(
+                1,
+                "mallardly",
+                "Mallard",
+                "Green",
+                5,
+                10,
+                "Quack",
+                true
+        );
+
         String line = duck.toLine();
-        String expected = "1,mallardly,Mallard";
+        String expected = "1,mallardly,Mallard,Green,5,10,Quack,true";
         assertEquals(expected, line);
     }
 
     @Test
     void fromLine() {
-        String line = "1,mallardly,Mallard";
-        DuckData expected = new DuckData(1,"mallardly", DuckType.MALLARD.toString());
+        String line = "1,mallardly,Mallard,Green,5,10,Quack,true";
+
         DuckData duck = DuckData.fromLine(line);
-        assertEquals(expected.id(), duck.id());
-        assertEquals(expected.type(), duck.type());
+
+        assertEquals(1, duck.id());
+        assertEquals("mallardly", duck.name());
+        assertEquals("Mallard", duck.type());
+        assertEquals("Green", duck.color());
+        assertEquals(5, duck.size());
+        assertEquals(10, duck.price());
+        assertEquals("Quack", duck.quackSound());
+        assertTrue(duck.flyBehavior());
     }
 }
